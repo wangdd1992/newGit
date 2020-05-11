85fe81a6c331adda57e185114fffc62617db54c9
wangdd1992 <www.1695786738@qq.com>
2020-05-11 17:23:46 +0800

    说啥是啥

---
+++ newBanches/newBanches/RootTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  RootTableViewCell.h
+//  newBanches
+//
+//  Created by Wangdandan on 2020/5/11.
+//  Copyright © 2020 Wangdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RootTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ newBanches/newBanches/RootTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  RootTableViewCell.m
+//  newBanches
+//
+//  Created by Wangdandan on 2020/5/11.
+//  Copyright © 2020 Wangdd. All rights reserved.
+//
+
+#import "RootTableViewCell.h"
+
+@implementation RootTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
